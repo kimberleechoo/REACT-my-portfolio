fb849ec66619855666f6244a6af2fb9f97a2d454
Kimberlee Choo <kimberlee.choo@growthops.asia>
2021-09-06 18:54:16 +0800

    react Tutorial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Hi Nice to meet you';
+  const likes = 50;
+  const link ='http://www.google.com';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+        <p>{"hello HELLO bonjour"}</p>
+        <p>{1,2,3,4,5}</p>
+        <p>{Math.random()*100}</p>
+        <a href={link}>GOOGLE :D </a>
+        
+      </div>
     </div>
   );
 }
